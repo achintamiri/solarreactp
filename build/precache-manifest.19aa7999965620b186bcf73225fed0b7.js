ddb1212ee221501ba9b6e884c6fe4df00191a071
achintamiri <achintamiri@users.noreply.github.com>
2019-11-15 18:29:34 +0000

    COde Refactoring App.js and data

---
+++ build/precache-manifest.19aa7999965620b186bcf73225fed0b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6891f561103fca8a8519788fecb46ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f5f773b1299243d4788",
+    "url": "/static/css/2.5a2f7233.chunk.css"
+  },
+  {
+    "revision": "777aea6d3c8549536194",
+    "url": "/static/css/main.10895230.chunk.css"
+  },
+  {
+    "revision": "9f5f773b1299243d4788",
+    "url": "/static/js/2.cec4eabc.chunk.js"
+  },
+  {
+    "revision": "777aea6d3c8549536194",
+    "url": "/static/js/main.39bbc9f1.chunk.js"
+  },
+  {
+    "revision": "0a963d52c081969410ba",
+    "url": "/static/js/runtime-main.4f088176.js"
+  }
+]);
